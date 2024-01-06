c1a02031e585cf5fe952155caebfa046d84482f8
Tagliassi <rafael.galafassi2005@gmail.com>
2024-01-06 12:30:30 -0300

    Class 01

---
+++ Exercise_01.cs
@@ -0,0 +1,20 @@
+﻿namespace Volvo_DotNet_Course;
+
+class Exercise_01
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("Digite o seu nome: ");
+        string nome = Console.ReadLine();
+
+        System.Console.Write("Digite a sua altura: ");
+        double altura = Convert.ToDouble(Console.ReadLine());
+
+        System.Console.Write("Digite o seu peso: ");
+        double peso = Convert.ToDouble(Console.ReadLine());
+
+        double imc = peso/(altura*altura);
+
+        System.Console.WriteLine($"{nome}, seu IMC é de: {imc:F2}");
+    }
+}
