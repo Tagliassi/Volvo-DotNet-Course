514628344598e1b1b56a6e5c10c04a73ffeb2c6d
Tagliassi <rafael.galafassi2005@gmail.com>
2024-01-08 17:45:00 -0300

    Exercícios da aula 01

---
+++ Class_01.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace Volvo_DotNet_Course
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            // Chama o método Executar da Exercise_01
+            //Exercise_01.Executar();
+
+            //Exercise_02.Executar();
+
+            //Exercise_03.Executar();
+
+            //Exercise_04.Executar();
+
+            //Exercise_05.Executar();
+
+            Exercise_06.Executar();
+        }
+    }
+
+    public class Exercise_01
+    {
+        public static void Executar()
+        {
+            System.Console.Write("Digite o seu nome: ");
+            string nome = Console.ReadLine();
+
+            System.Console.Write("Digite a sua altura: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            System.Console.Write("Digite o seu peso: ");
+            double peso = Convert.ToDouble(Console.ReadLine());
+
+            double imc = peso / (altura * altura);
+
+            System.Console.WriteLine($"{nome}, seu IMC é de: {imc:F2}");
+        }
+    }
+
+    public class Exercise_02
+    {
+        public static void Executar()
+        {
+            System.Console.Write("Digite o seu ano de nascimento: ");
+            int ano = Convert.ToInt32(Console.ReadLine());
+
+            int idade = (2024 - ano);
+
+            System.Console.WriteLine($"Sua idade é {idade}");
+        }
+    }
+
+    public class Exercise_03
+    {
+        public static void Executar()
+        {
+            
+            double valor_locacao = 180.00;
+
+            System.Console.Write("Digite o quantos dias quer alugar este carro: ");
+            int dias = Convert.ToInt32(Console.ReadLine());
+
+            double valor_total = (dias*valor_locacao);
+
+            System.Console.WriteLine($"O valor a ser pago ao alugar este carro por {dias} dia(s) é: R${valor_total} reais");
+        }
+    }
+
+    public class Exercise_04
+    {
+        public static void Executar()
+        {
+
+            System.Console.Write("Digite a temperatura em celcius: ");
+            double temperatura = Convert.ToDouble(Console.ReadLine());
+
+            double temperatura_convertida = (temperatura * 9/5) + 32;
+
+            System.Console.WriteLine($"O valor da temperatura em Celcius para Fahrenheit é: {temperatura_convertida}");
+        }
+    }
+
+    public class Exercise_05
+    {
+        public static void Executar()
+        {
+
+            System.Console.Write("Digite a  primeira nota: ");
+            double nota_1 = Convert.ToDouble(Console.ReadLine());
+
+            System.Console.Write("Digite a  segunda nota: ");
+            double nota_2 = Convert.ToDouble(Console.ReadLine());
+
+            System.Console.Write("Digite a  terceira nota: ");
+            double nota_3 = Convert.ToDouble(Console.ReadLine());
+
+            System.Console.Write("Digite a  quarta nota: ");
+            double nota_4 = Convert.ToDouble(Console.ReadLine());
+
+            double soma_notas = nota_1 + nota_2 + nota_3 + nota_4;
+
+            double media_notas = soma_notas/4;
+
+            System.Console.WriteLine($"O valor da média de notas é: {media_notas}");
+        }
+    }
+
+        public class Exercise_06
+    {
+        public static void Executar()
+        {
+
+            System.Console.Write("Digite a  sua idade: ");
+            int idade = Convert.ToInt32(Console.ReadLine());
+
+            int idade_meses = idade*12;
+
+            System.Console.WriteLine($"A sua idade em meses é: {idade_meses}");
+        }
+    }
+
+}
--- obj/Debug/net8.0/Volvo-DotNet-Course.AssemblyInfo.cs
+++ obj/Debug/net8.0/Volvo-DotNet-Course.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Volvo-DotNet-Course")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+440ac773a7de607d40e74c3a7f79149a7b12ca83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1a02031e585cf5fe952155caebfa046d84482f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Volvo-DotNet-Course")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Volvo-DotNet-Course")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
